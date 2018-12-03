355736005b868ee9e5241c6da2bc7c1cd308b050
lixd <dandan580826@163.com>
2018-12-03 14:03:52 +0800

    js任务完成

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var minCss = require('gulp-clean-css');
 
 var concat = require('gulp-concat');
 
+var uglify = require('gulp-uglify');
+
 gulp.task('server',function(){
     browserSync({
         server:{
@@ -21,11 +23,20 @@ gulp.task('server',function(){
     })
 })
 
+//css
 gulp.task('devScss',function(){
     return gulp.src('./src/scss/*.scss')
     .pipe(sass())
     .pipe(concat('all.css'))
     .pipe(minCss())
     .pipe(gulp.dest('./src/css'))
+})
+
+//js
+gulp.task('devJs',function(){
+    return gulp.src('./src/js/*.js')
+    .pipe(uglify())
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./src/js/minJs'))
 })
 
